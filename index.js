efcefa5940db181012eb837eb53fe71935b00934
Gunay <gunaykaraoglu21@gmail.com>
2023-10-28 00:22:38 +0300

    Added a html form

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port, () => console.log(`App live at http://localhost:${port}/`));
+
 app.get('/', (req, res) => {
     res.render("index.ejs")
 });
-app.listen(port, () => console.log(`App live at http://localhost:${port}/`));
+
+app.post("/submit", (req, res) => {
+  
+  console.log(req.body.barcode);
+
+  res.redirect("/")
+})
