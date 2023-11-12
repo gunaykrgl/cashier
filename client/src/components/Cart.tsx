746af39ad3093213bb8aaf34d37c8930593cb7ae
Gunay <gunaykaraoglu21@gmail.com>
2023-11-12 23:58:58 +0300

    Moved styling to external css

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -1,28 +1,29 @@
 import React from "react"
 import { useTable } from 'react-table'
+import "./Cart.css"
 
 export default function Cart(props: any) {
     const itemsArray = Object.values(props.items)
-    console.log(itemsArray)
+    
     return (
         <div className="cartTable">
             <table>
                 <thead>
-                    <tr style={{ borderBottom: '2px solid black' }}>
-                        <th style={{ padding: '10px', textAlign: 'left' }}>Item</th>
-                        <th style={{ padding: '10px', textAlign: 'left' }}>Quantity</th>
-                        <th style={{ padding: '10px', textAlign: 'left' }}>Per Item Price</th>
-                        <th style={{ padding: '10px', textAlign: 'left' }}>Price</th>
+                    <tr>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Per Item Price</th>
+                        <th>Price</th>
                     </tr>
                 </thead>
                 <tbody>
                     {itemsArray.map((item: any) => (
-                            <tr style={{ borderBottom: '1px solid black' }}>
-                                <td style={{ padding: '10px', textAlign: 'left' }}>{item.name}</td>
-                                <td style={{ padding: '10px', textAlign: 'left' }}>{item.quantity}</td>
-                                <td style={{ padding: '10px', textAlign: 'left' }}>{item.price}</td>
-                                <td style={{ padding: '10px', textAlign: 'left' }}>{item.price * item.quantity}</td>
-                            </tr>
+                        <tr>
+                            <td>{item.name}</td>
+                            <td>{item.quantity}</td>
+                            <td>{item.price}</td>
+                            <td>{item.price * item.quantity}</td>
+                        </tr>
                     ))}
                 </tbody>
             </table>
