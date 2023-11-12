386c387addcb350cb28a4703e0b94718df789562
Gunay <gunaykaraoglu21@gmail.com>
2023-11-12 23:41:29 +0300

    Added a table that shows the current cart

---
+++ client/src/components/Cart.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default function Cart(props: any) {
+    const itemsArray = Object.values(props.items)
+    console.log(itemsArray)
+    return (
+        <div className="cartTable">
+            <table>
+                <thead>
+                    <tr style={{ borderBottom: '2px solid black' }}>
+                        <th style={{ padding: '10px', textAlign: 'left' }}>Item</th>
+                        <th style={{ padding: '10px', textAlign: 'left' }}>Quantity</th>
+                        <th style={{ padding: '10px', textAlign: 'left' }}>Per Item Price</th>
+                        <th style={{ padding: '10px', textAlign: 'left' }}>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {itemsArray.map((item: any) => (
+                            <tr style={{ borderBottom: '1px solid black' }}>
+                                <td style={{ padding: '10px', textAlign: 'left' }}>{item.name}</td>
+                                <td style={{ padding: '10px', textAlign: 'left' }}>{item.quantity}</td>
+                                <td style={{ padding: '10px', textAlign: 'left' }}>{item.price}</td>
+                                <td style={{ padding: '10px', textAlign: 'left' }}>{item.price * item.quantity}</td>
+                            </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
