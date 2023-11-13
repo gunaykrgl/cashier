31d0e59c4c75b83a8cb1963da7c4756bd16b1c4d
Gunay <gunaykaraoglu21@gmail.com>
2023-11-13 12:57:36 +0300

    Moved navbar to its own component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Cart from "./components/Cart"
 import BarcodeForm from './components/BarcodeForm'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <>
-      <h1>Cashier</h1>
+    <Navbar />
       <div className='barcodePage'>
         <BarcodeForm />
         <Cart items= {[{name: "demo", quantity:"2", price:"5"}]}/>
