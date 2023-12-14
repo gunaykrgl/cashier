3cdf8b4f22c9bb20b79b2f164a0101d353618946
Gunay <gunaykaraoglu21@gmail.com>
2023-12-14 18:25:45 +0300

    Update database source in config and productDatabase files

--- api/config.ts
+++ api/config.ts
@@ -1,12 +1,14 @@
-// config.js
 import path from 'path';
 
 interface Config {
   projectRoot: string;
+  DBSOURCE: string;
+
 }
 
 const config: Config = {
   projectRoot: path.resolve(__dirname),
+  DBSOURCE: path.join(path.resolve(__dirname), "db", "db.sqlite")
 }
 
 export default config;
--- api/db/Handlers/productDatabase.ts
+++ api/db/Handlers/productDatabase.ts
@@ -1,10 +1,8 @@
 import path from "path"
 import config from "../../config.ts"
 
-const DBSOURCE = path.join(config.projectRoot, "db", "db.sqlite")
-
 import sqliteHandler from "./sqliteHandler.ts" 
 
-const sqliteHandlerObj = new sqliteHandler(DBSOURCE, "product")
+const sqliteHandlerObj = new sqliteHandler(config.DBSOURCE, "product")
 
 module.exports = sqliteHandlerObj
