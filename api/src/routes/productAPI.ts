ba9869110a7765a1d4be25be5fc1988949fb90b1
Gunay <gunaykaraoglu21@gmail.com>
2023-12-18 14:48:29 +0300

    Add query endpoint to productAPI.ts

--- api/src/routes/productAPI.ts
+++ api/src/routes/productAPI.ts
@@ -1,10 +1,7 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { join } from "path";
 import config from "../../config";
-import sqliteHandler from "../../db/Handlers/sqliteHandler.ts" 
-
-// import interfaces
-import { IProduct } from '../interfaces/product.interface.ts';
+import sqliteHandler from "../../db/Handlers/sqliteHandler.ts";
 
 // const sqliteHandler: any = require(join(config.projectRoot, "db", "Handlers", "sqliteHandler.ts"));
 const db: any = new sqliteHandler(config.DBSOURCE, "product");
@@ -40,5 +37,16 @@ router.get("/getProduct", async (req: Request, res: Response) => {
   }
 });
 
+// Query products
+router.get("/query", async (req: Request, res: Response) => {
+  const params: { [key: string]: any } = req.query;
+  try {
+    const rows = await db.query(params);
+    res.status(200).json(rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 export default router;
