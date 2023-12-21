d7aa1f12d333a8229fb116a226846b7e8a3d20de
Gunay <gunaykaraoglu21@gmail.com>
2023-12-21 17:52:33 +0300

    add stock validation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,8 @@ interface Product {
 
 function App() {
   const [cart, setCart] = useState<Product[]>([])
+
+  // Fetch the products list from the server
   const [productsList, setProductsList] = useState<Product[]>([])
   useEffect(() => {
     fetch('http://localhost:9000/getProductsList')
@@ -21,13 +23,18 @@ function App() {
 
   function handleSubmit(event: any) {
     event.preventDefault()
-    const barcode:Number = Number(event.target.barcode.value)
+    const barcode: Number = Number(event.target.barcode.value)
     //! VALIDATE BARCODE HERE (int, string etc)
-    const product = productsList.find((product) => product.barcode == barcode)
+    const product: Product | undefined = productsList.find((product) => product.barcode == barcode)
     // Check if the product is in the cart
-    const isProductInCart = cart.find(product => product.barcode === barcode);
+    const productInCart = cart.find(product => product.barcode === barcode);
 
-    if (isProductInCart) {
+    if (productInCart?.quantity >= product?.quantity) {
+      alert('Not enough stock')
+      return
+    }
+
+    if (productInCart) {
       const updatedCart = cart.map(product => {
         if (product.barcode === barcode) {
           return {
@@ -43,51 +50,52 @@ function App() {
       setCart([...cart, { ...product, quantity: 1 } as Product])
     }
 
-    //! CHANGE THE NEXT LINE, IT'S JUST A LAZY WAY OF CLEANING THE FORM
-    event.target.barcode.value = ''
-  }
-  function finalizePurchase(cart: any) {
-    const productsToSend = cart.map((product: any) => {
-      return {
-        barcode: product.barcode,
-        quantity: product.quantity
-      }
-    })
-    console.log(productsToSend)
-    fetch('http://localhost:9000/finalizeCart', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(productsToSend)
-    })
 
-    setCart([])
-  }
-  return (
-    <>
-      <Navbar />
-      <div className='barcodePage'>
-        <div className='barcodeLookup'>
-          <p>Enter the barcode below to add a product to cart:</p>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="number"
-              name="barcode"
-              autoComplete="off"
-              placeholder="barcode"
+  //! CHANGE THE NEXT LINE, IT'S JUST A LAZY WAY OF CLEANING THE FORM
+  event.target.barcode.value = ''
+}
+function finalizePurchase(cart: any) {
+  const productsToSend = cart.map((product: any) => {
+    return {
+      barcode: product.barcode,
+      quantity: product.quantity
+    }
+  })
+  console.log(productsToSend)
+  fetch('http://localhost:9000/finalizeCart', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(productsToSend)
+  })
 
-              autoFocus
-              required
-            />
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-        <Cart items={cart} />
-        <button onClick={() => finalizePurchase(cart)}>Complete</button>
+  setCart([])
+}
+return (
+  <>
+    <Navbar />
+    <div className='barcodePage'>
+      <div className='barcodeLookup'>
+        <p>Enter the barcode below to add a product to cart:</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            name="barcode"
+            autoComplete="off"
+            placeholder="barcode"
+
+            autoFocus
+            required
+          />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
-    </>
-  )
+      <Cart items={cart} />
+      <button onClick={() => finalizePurchase(cart)}>Complete</button>
+    </div>
+  </>
+)
 }
 
 export default App
