f1e1efa9c5fd6818f2a9de8ec9e05f2be16a7b73
Gunay <gunaykaraoglu21@gmail.com>
2023-12-25 13:12:07 +0300

    Add react-router-dom and move Cashier page to a seperate file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,19 @@
-import './App.css'
 import Cart from "./components/Cart"
 import BarcodeForm from './components/BarcodeForm'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import { useState, useEffect } from 'react'
+import Cashier from './pages/Cashier';
 
 function App() {
-  const [cart, setCart] = useState<Product[]>([])
-  
-  //! REFACTOR: This is a hack to force the productsList to refresh
-  const [shouldRefresh, setShouldRefresh] = useState<boolean>(false)
-  
-  // Fetch the products list from the server
-  const [productsList, setProductsList] = useState<Product[]>([])
-  useEffect(() => {
-    fetch('http://localhost:9000/getProductsList')
-      .then((res) => res.json())
-      .then((data) => setProductsList(data))
-  }, [shouldRefresh])
-
-
-  function finalizePurchase(cart: any) {
-    const productsToSend = cart.map((product: any) => {
-      return {
-        barcode: product.barcode,
-        quantity: product.quantity
-      }
-    })
-    console.log(productsToSend)
-    fetch('http://localhost:9000/finalizeCart', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(productsToSend)
-    })
-    setShouldRefresh(!shouldRefresh)
-    setCart([])
-  }
-
-  const removeItem = (itemId: number) => {
-    const updatedCart = cart.filter(product => product.barcode !== itemId)
-    setCart(updatedCart)
-  }
 
   return (
-    <>
+    <Router>
       <Navbar />
-      <div className='mainContainer'>
-
-        <BarcodeForm
-          productsList={productsList}
-          cart={cart}
-          setCart={setCart}
-        />
-        <Cart items={cart}
-          removeItem={removeItem}
-        />
-        <button onClick={() => finalizePurchase(cart)}>Complete</button>
-      </div>
-    </>
+      <Routes>
+        <Route path='/' element={<Cashier />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/pages/Cashier.tsx
@@ -0,0 +1,64 @@
+import './Cashier.css'
+import Cart from "../components/Cart"
+import BarcodeForm from '../components/BarcodeForm'
+
+import { useState, useEffect } from 'react'
+
+function Cashier() {
+  const [cart, setCart] = useState<Product[]>([])
+  
+  //! REFACTOR: This is a hack to force the productsList to refresh
+  const [shouldRefresh, setShouldRefresh] = useState<boolean>(false)
+  
+  // Fetch the products list from the server
+  const [productsList, setProductsList] = useState<Product[]>([])
+  useEffect(() => {
+    fetch('http://localhost:9000/getProductsList')
+      .then((res) => res.json())
+      .then((data) => setProductsList(data))
+  }, [shouldRefresh])
+
+
+  function finalizePurchase(cart: any) {
+    const productsToSend = cart.map((product: any) => {
+      return {
+        barcode: product.barcode,
+        quantity: product.quantity
+      }
+    })
+    console.log(productsToSend)
+    fetch('http://localhost:9000/finalizeCart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(productsToSend)
+    })
+    setShouldRefresh(!shouldRefresh)
+    setCart([])
+  }
+
+  const removeItem = (itemId: number) => {
+    const updatedCart = cart.filter(product => product.barcode !== itemId)
+    setCart(updatedCart)
+  }
+
+  return (
+    <>
+      <div className='mainContainer'>
+
+        <BarcodeForm
+          productsList={productsList}
+          cart={cart}
+          setCart={setCart}
+        />
+        <Cart items={cart}
+          removeItem={removeItem}
+        />
+        <button onClick={() => finalizePurchase(cart)}>Complete</button>
+      </div>
+    </>
+  )
+}
+
+export default Cashier
