290461a969b19d1627ae2c0fa59ba2d6792acc3f
Gunay <gunaykaraoglu21@gmail.com>
2023-12-26 11:12:02 +0300

    remove unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
-import { useState, useEffect } from 'react'
+
 import Cashier from './pages/Cashier';
 
 function App() {
