51942284b2bdc3bd6f15e2d288fafd6eec8fb343
Gunay <gunaykaraoglu21@gmail.com>
2023-12-30 13:19:23 +0300

    created ManageProducts route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/Layout/MainLayout';
 import Cashier from './pages/Cashier';
 import Login from './pages/Login';
-import AddProduct from './pages/addProduct';
+import ManageProducts from './pages/ManageProducts';
+// import AddProduct from './components/addProduct';
 
 function App() {
 
@@ -16,7 +17,8 @@ function App() {
           <Route path='/login' element={<Login />} />
           
           {/* //! CHANGE this so that add product and update product is in the same page  */}
-          <Route path='/addProduct' element={<AddProduct />} />
+          {/* <Route path='/addProduct' element={<AddProduct />} /> */}
+          <Route path='/manageProducts' element={<ManageProducts />} />
         </Route>
         
       </Routes>
---
+++ client/src/pages/ManageProducts.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+
+    //! PUT A GRID OF PRODUCTS AND UPDATE AND DELETE BUTTONS
+    //! ALSO PUT A + BUTTON THAT ADDS A NEW PRODUCT
+    return (
+        <>
+        </>
+    )
+}
+
